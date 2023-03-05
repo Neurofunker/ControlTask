7ccdb49cee9565531314c2c68a675de6db223c79
Makc <m_stefanenkov@mail.ru>
2023-03-05 15:45:04 +0100

    Добавил решение задачи

--- ControlTask/Program.cs
+++ ControlTask/Program.cs
@@ -2,3 +2,29 @@
 // длина которых меньше либо равна 3 символа. Первоначальный массив можно ввести с клавиатуры, 
 // либо задать на старте выполнения алгоритма. При решение не рекомендуется пользоваться коллекциями, 
 // лучше обойтись исключительно массивам
+
+string[] array1 = new string[6] {"12345", "abc", "Max", "11", "qwerty", "666"};
+string[] array2 = new string[array1.Length];
+
+void ResultArray(string[] array1, string[] array2)
+{
+    int result = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[result] = array1[i];
+        result++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+ResultArray(array1, array2);
+PrintArray(array2);
